3dba2aaba8ad4532408f65b888e5f58c6927b269
Tomas Perez <tomasitopg6@gmail.com>
2020-07-03 08:49:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    project.css("h2.bbcard_name strong a").text
   end
   
   projects
